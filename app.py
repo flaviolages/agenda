4597caf95f450e4be3d33a99b1eb20542e5030b8
sol <sol@servidor.servidor.soldecor.local>
2019-11-27 15:03:37 -0300

    Forms

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, redirect, request
+from datetime import datetime
+import sqlite3
+
+
 
 
 app = Flask('app')
+con = sqlite3.connect('agenda.db')
+cursor = con.cursor()
+
+
+
+@app.route('/gravar', methods=['POST'])
+def gravar():
+	nome = request.form['nome']
+	horaAgenda = request.form['horaAgenda']
+	dataAgenda = request.form['dataAgenda']
+	local = request.form['local']
+	telefone = request.form['telefone']
+	tags = request.form['tags']
+
+	datahora = datetime.now()
+	dataLog = datahora.strftime('%d/%m/%Y %H:%M')
+
+	tupla = (nome, horaAgenda, dataAgenda, local, telefone, tags, dataLog)
+	
+	cursor.execute("INSER INTO agenda(nome, hora,data, local, telefone, tags, logData) values ?;")
+
+	print(tupla)
+	return redirect('/agendar')
+
+
+
+
 
 
 
 
 @app.route('/agendar')
-def agendamento():
-	return render_template('agendamento.html')
+def agendar():
+	return render_template('agendar.html')
+
+
+
 
 
 
