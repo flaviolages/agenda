3aa161170e0a52c59a44ceaa3c2f1df4bc727409
sol <sol@servidor.servidor.soldecor.local>
2019-11-27 17:36:24 -0300

    Atualizações

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import sqlite3
 
 app = Flask('app')
 
+con = sqlite3.connect('agenda.db')
+c = con.cursor()
+
+
 
 @app.route('/gravar', methods=['POST'])
 def gravar():
@@ -16,28 +20,18 @@ def gravar():
 	local = request.form['local']
 	telefone = request.form['telefone']
 	tags = request.form['tags']
-
 	datahora = datetime.now()
 	dataLog = datahora.strftime('%d/%m/%Y %H:%M')
-
 	tupla = (nome, horaAgenda, dataAgenda, local, telefone, tags, dataLog)
-	dados = [tupla]
+	
 	print(tupla)
-	print(dados)
 	return redirect('/agendar')
-
-
-
 
 
 
 @app.route('/agendar')
 def agendar():
 	return render_template('agendar.html')
-
-
-
-
 
 
 app.run()
--- gravadb.py
+++ gravadb.py
@@ -5,11 +5,12 @@ con = sqlite3.connect('agenda.db')
 c = con.cursor()
 
 
-dados = ('Teste 1', '03:02', '2019-11-27', 'Local teste', '41999999999', 'tags tags2', '27/11/2019 15:12')
+dados = ('2w','Teste 1', '03:02', '2019-11-27', 'Local teste', '41999999999', 'tags tags2', '27/11/2019 15:12')
 
-c.execute("INSERT INTO agenda(nome, hora,data, local, telefone, tags, logData) VALUES ('Teste 1', '03:02', '2019-11-27', 'Local teste', '41999999999', 'tags tags2', '27/11/2019 15:12');")
+#c.execute("INSERT INTO agenda VALUES ('{}', '03:02', '2019-11-27', 'Local teste', '41999999999', 'tags tags2', '27/11/2019 15:12')",dados[1])
+
+c.executemany("INSERT INTO agenda VALUES (?,?,?,?,?,?,?,?);", dados)
 
 
-	
 con.commit()
 con.close()
